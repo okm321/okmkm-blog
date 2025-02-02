efefb220964ab9eff98ee7213f773add6ce93984
okm321 <okmkm321@gmail.com>
2025-02-02 18:43:08 +0900

    feat: スマホ時の考慮

--- app/src/components/shared/Introduction/Introduction.tsx
+++ app/src/components/shared/Introduction/Introduction.tsx
@@ -10,47 +10,48 @@ export const Introduction = () => {
 				<div className={classes.profile__image} />
 				<p>Hello, I'm a web developer!</p>
 				<p>Here's my tech stack!</p>
-			<div className={classes.modelViewer}>
-				<div className={`${classes.modelViewer__item}`}>
-					<ModelViewer
-						src={"/react-pi.glb"}
-						interactionPrompt="none"
-						disableZoom
-					/>
+				<div className={classes.modelViewer}>
+					<div className={`${classes.modelViewer__item}`}>
+						<ModelViewer
+							src={"/react-pi.glb"}
+							interactionPrompt="none"
+							disableZoom
+							cameraOrbit="140deg 90deg 105%"
+						/>
+					</div>
+					<div className={`${classes.modelViewer__item}`}>
+						<ModelViewer
+							src={"/next-pi.glb"}
+							interactionPrompt="none"
+							cameraOrbit="160deg 90deg 105%"
+							disableZoom
+						/>
+					</div>
+					<div className={`${classes.modelViewer__item}`}>
+						<ModelViewer
+							src={"/go-pi.glb"}
+							interactionPrompt="auto"
+							cameraOrbit="180deg 90deg 105%"
+							disableZoom
+						/>
+					</div>
+					<div className={`${classes.modelViewer__item}`}>
+						<ModelViewer
+							src={"/vue-pi.glb"}
+							interactionPrompt="none"
+							cameraOrbit="200deg 90deg 105%"
+							disableZoom
+						/>
+					</div>
+					<div className={`${classes.modelViewer__item}`}>
+						<ModelViewer
+							src={"/nuxt-pi.glb"}
+							interactionPrompt="none"
+							cameraOrbit="220deg 90deg 105%"
+							disableZoom
+						/>
+					</div>
 				</div>
-				<div className={`${classes.modelViewer__item}`}>
-					<ModelViewer
-						src={"/next-pi.glb"}
-						interactionPrompt="none"
-						cameraOrbit="160deg 90deg 105%"
-						disableZoom
-					/>
-				</div>
-				<div className={`${classes.modelViewer__item}`}>
-					<ModelViewer
-						src={"/go-pi.glb"}
-						interactionPrompt="none"
-						cameraOrbit="180deg 90deg 105%"
-						disableZoom
-					/>
-				</div>
-				<div className={`${classes.modelViewer__item}`}>
-					<ModelViewer
-						src={"/vue-pi.glb"}
-						interactionPrompt="none"
-						cameraOrbit="200deg 90deg 105%"
-						disableZoom
-					/>
-				</div>
-				<div className={`${classes.modelViewer__item}`}>
-				<ModelViewer
-						src={"/nuxt-pi.glb"}
-						interactionPrompt="none"
-						cameraOrbit="220deg 90deg 105%"
-						disableZoom
-					/>
-				</div>
-			</div>
 			</div>
 		</div>
 	);
