d8382d3ada772fdf70b589b32ce7c39d35cf7b1c
okm321 <okmkm321@gmail.com>
2025-02-03 01:48:51 +0900

    fix: outputFileTracingRoot

--- app/next.config.ts
+++ app/next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
 		implementation: "sass-embedded", // sassの代わりにsass-embeddedを使用
 	},
 	outputFileTracingRoot: path.join(__dirname, ".."),
+	outputFileTracingIncludes: {
+		"/articles/[slug]/opengraph-image": [
+			path.join("..", "contents-manager", "articles", "**/*"),
+			path.join("..", "contents-manager", "images", "**/*"),
+		],
+	},
 	output: "standalone",
 };
 
