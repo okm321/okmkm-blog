061bffd6ad6b5dcab0a059034af853197dec34cc
okm321 <okmkm321@gmail.com>
2025-05-06 06:01:58 +0900

    fix: hamburger-design

--- app/src/components/shared/Preload/Preload.tsx
+++ app/src/components/shared/Preload/Preload.tsx
@@ -3,6 +3,8 @@ export function Preload() {
 		<>
 			<link rel="preload" href="/9pin.png" as="image" type="image/png" />
 			<link rel="preload" href="/5pin.png" as="image" type="image/png" />
+			<link rel="preload" href="/9pin_dark.png" as="image" type="image/png" />
+			<link rel="preload" href="/5pin_dark.png" as="image" type="image/png" />
 			<link
 				rel="preload"
 				href="/theme-switch_light.svg"
