2119fc859e3b0a6519bfc180dfc3ada3a7b5ee79
okm321 <okmkm321@gmail.com>
2025-05-05 22:48:34 +0900

    add: preload img

--- app/src/components/shared/Preload/Preload.tsx
+++ app/src/components/shared/Preload/Preload.tsx
@@ -1,17 +1,19 @@
 export function Preload() {
 	return (
 		<>
-			<link rel="preload" href="/9pin.svg" as="image" type="image/svg+xml" />
+			<link rel="preload" href="/9pin.png" as="image" type="image/png" />
+			<link rel="preload" href="/9pin_dark.png" as="image" type="image/png" />
+			<link rel="preload" href="/5pin.png" as="image" type="image/png" />
+			<link rel="preload" href="/5pin_dark.png" as="image" type="image/png" />
 			<link
 				rel="preload"
-				href="/9pin_dark.svg"
+				href="/theme-switch_light.svg"
 				as="image"
 				type="image/svg+xml"
 			/>
-			<link rel="preload" href="/5pin.svg" as="image" type="image/svg+xml" />
 			<link
 				rel="preload"
-				href="/5pin_dark.svg"
+				href="/theme-switch_dark.svg"
 				as="image"
 				type="image/svg+xml"
 			/>
