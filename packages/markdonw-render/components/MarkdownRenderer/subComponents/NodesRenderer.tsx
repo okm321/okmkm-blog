56162b5f1fdc55fadd1ceb9228c353e951981f59
okm321 <okmkm321@gmail.com>
2025-12-23 17:01:04 +0900

    fix: lint

--- packages/markdonw-render/components/MarkdownRenderer/subComponents/NodesRenderer.tsx
+++ packages/markdonw-render/components/MarkdownRenderer/subComponents/NodesRenderer.tsx
@@ -19,76 +19,76 @@ import { TextNode } from "./TextNode";
 import { ThemanticBreakNode } from "./ThemanticBreakNode";
 
 export const NodesRenderer: FC<{ nodes: RootContent[] }> = ({ nodes }) => {
-  return nodes.map((node, index) => {
-    switch (node.type) {
-      case "heading": {
-        return <HeadingNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "text": {
-        return <TextNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "paragraph": {
-        return <ParagraphNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "inlineCode": {
-        return <InlineCodeNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "blockquote": {
-        return <BlockQuoteNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "link": {
-        return <LinkNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "list": {
-        return <ListNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "listItem": {
-        return <ListItemNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "strong": {
-        return <StrongNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "image": {
-        return <ImageNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "code": {
-        // @ts-ignore Server Component
-        return <CodeNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "delete": {
-        return <DeleteNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "table": {
-        return <TableNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "thematicBreak": {
-        return <ThemanticBreakNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "html": {
-        return <HTMLNode key={`${node.type}-${index}`} node={node} />;
-      }
-      case "link-card": {
-        return (
-          <Suspense
-            key={`${node.type}-${index}`}
-            fallback={<LinkCardSkeleton />}
-          >
-            {/* @ts-ignore Server Component */}
-            <LinkCard key={`${node.type}-${index}`} node={node} />
-          </Suspense>
-        );
-      }
-      default: {
-        if (process.env.NODE_ENV === "development") {
-          return (
-            <div key={`${node.type}-${index}`}>
-              <p style={{ color: "red" }}>Unknown node type: {node.type}</p>
-              <pre>{JSON.stringify(node, null, 2)}</pre>
-            </div>
-          );
-        }
-        throw new Error(`Unknown node type: ${node.type}`);
-      }
-    }
-  });
+	return nodes.map((node, index) => {
+		switch (node.type) {
+			case "heading": {
+				return <HeadingNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "text": {
+				return <TextNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "paragraph": {
+				return <ParagraphNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "inlineCode": {
+				return <InlineCodeNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "blockquote": {
+				return <BlockQuoteNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "link": {
+				return <LinkNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "list": {
+				return <ListNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "listItem": {
+				return <ListItemNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "strong": {
+				return <StrongNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "image": {
+				return <ImageNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "code": {
+				// @ts-ignore Server Component
+				return <CodeNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "delete": {
+				return <DeleteNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "table": {
+				return <TableNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "thematicBreak": {
+				return <ThemanticBreakNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "html": {
+				return <HTMLNode key={`${node.type}-${index}`} node={node} />;
+			}
+			case "link-card": {
+				return (
+					<Suspense
+						key={`${node.type}-${index}`}
+						fallback={<LinkCardSkeleton />}
+					>
+						{/* @ts-ignore Server Component */}
+						<LinkCard key={`${node.type}-${index}`} node={node} />
+					</Suspense>
+				);
+			}
+			default: {
+				if (process.env.NODE_ENV === "development") {
+					return (
+						<div key={`${node.type}-${index}`}>
+							<p style={{ color: "red" }}>Unknown node type: {node.type}</p>
+							<pre>{JSON.stringify(node, null, 2)}</pre>
+						</div>
+					);
+				}
+				throw new Error(`Unknown node type: ${node.type}`);
+			}
+		}
+	});
 };
